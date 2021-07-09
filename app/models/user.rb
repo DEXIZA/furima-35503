bb4d3a38f0b60b7274575bf1af138b322aae00e9
DEXIZA <dexizaia@gmail.com>
2021-07-09 17:04:57 +0900

    userへのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
 
-  
-
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+      # 全角カナの区別
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      # 全角かな/カナ漢字の区別
+      validates :last_name
+      validates :first_name
+    end
+  end
   # ユーザー管理機能に於いて、空を禁止する
   # validates :カラム名, バリデーションの種類
-
-  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  # passwordの英数字混合の判断
+  validates_format_of :password, with: PASSWORD_REGEX
 end
