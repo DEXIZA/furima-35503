6901517299acaa78a77b9ac8e5cc2a00f503dde4
DEXIZA <dexizaia@gmail.com>
2021-07-31 17:57:36 +0900

    モデル関係でかなりの間違いがあったので修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,11 +6,20 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+    # if @item.save
+    #   redirect_to root_path
+    # else
+    #   render new_item_path
+    # end
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:aaaaaaaaaa, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name,:description,:price,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:shipping_time_id,:image).merge(user_id: current_user.id)
   end
-end
 
+end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,23 +2,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_charge
-  belongs_to :prefecture
-  belongs_to :shipping_time
 
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :price
+    validates :price,numericality: true
     validates :user_id
+    with_options presence: true, numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id 
+      validates :shipping_charge_id 
+      validates :prefecture_id 
+      validates :shipping_time_id     
+    end
   end
-
-  validates :category, numericality: { other_than: 1 } 
-  validates :condition, numericality: { other_than: 1 } 
-  validates :shipping_charge, numericality: { other_than: 1 } 
-  validates :prefecture, numericality: { other_than: 1 } 
-  validates :shipping_time, numericality: { other_than: 1 } 
-
 end
