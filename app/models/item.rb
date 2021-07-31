f1c4174dde94b0047d4d1366267137594aa79eff
DEXIZA <dexizaia@gmail.com>
2021-07-31 15:19:48 +0900

    article　モデルは間違いだった為itmsに統合

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_time
 
   with_options presence: true do
     validates :item_name
@@ -10,4 +14,11 @@ class Item < ApplicationRecord
     validates :price
     validates :user_id
   end
+
+  validates :category, numericality: { other_than: 1 } 
+  validates :condition, numericality: { other_than: 1 } 
+  validates :shipping_charge, numericality: { other_than: 1 } 
+  validates :prefecture, numericality: { other_than: 1 } 
+  validates :shipping_time, numericality: { other_than: 1 } 
+
 end
