3fa96b22353cb0290b3fd757342a85c37080409a
DEXIZA <dexizaia@gmail.com>
2021-08-07 13:52:07 +0900

    リダレクト処理と価格のバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
   end
 
@@ -20,6 +22,7 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name,:description,:price,:category_id,:condition_id,:shipping_charge_id,:prefecture_id,:shipping_time_id,:image).merge(user_id: current_user.id)
   end
+
 
 end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :price,numericality: true
+    validates :price,numericality: true , inclusion: { in:300.. 9999999 }
     validates :user_id
     with_options presence: true, numericality: { other_than: 1 } do
       validates :category_id
