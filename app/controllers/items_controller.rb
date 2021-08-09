9cada1b3cc29369c082844d8c5e4b4e857768367
DEXIZA <dexizaia@gmail.com>
2021-08-09 23:39:49 +0900

    ロボコップコード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,16 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :description, :price, :category_id, :condition_id, :shipping_charge_id, :prefecture_id,
-                                 :shipping_time_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(
+      :item_name,
+      :description,
+      :price, :category_id,
+      :condition_id,
+      :shipping_charge_id,
+      :prefecture_id,
+      :shipping_time_id,
+      :image
+    )
+          .merge(user_id: current_user.id)
   end
 end
