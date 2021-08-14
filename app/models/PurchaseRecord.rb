eae2253c448a198f1953f59b5c2e1ae9e1e36594
DEXIZA <dexizaia@gmail.com>
2021-08-14 12:34:42 +0900

    ダミー画像非表示実装の為の前準備

--- app/models/PurchaseRecord.rb
+++
@@ -1,4 +0,0 @@
-class PurchaseRecord < ApplicationRecord
-  belongs_to :item
-  belongs_to :user
-end
--- app/models/item.rb
+++ app/models/item.rb
@@ -19,4 +19,6 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :category
   belongs_to :prefecture
+  belongs_to :shipping_time
+  belongs_to :condition
 end
