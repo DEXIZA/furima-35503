6baa062a2c2ed5641954a6a883a242299e366d57
DEXIZA <dexizaia@gmail.com>
2021-08-22 13:18:09 +0900

    マイグレファイル作成

--- db/migrate/20210821094649_create_shipping_add_infos.rb
+++ db/migrate/20210821094649_create_shipping_add_infos.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddInfos < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_add_infos do |t|
-
+      t.string :postal_code,  null: false
+      t.integer :prefecture_id,  null: false
+      t.string :municipality,  null: false
+      t.string :street_address,  null: false
+      t.string :building_name
+      t.string :street_address,  null: false
+      t.references :purchase_record,  null: false, foreign_key: true
       t.timestamps
     end
   end
