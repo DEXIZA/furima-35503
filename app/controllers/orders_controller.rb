04ce7f2869cfec80cd5f707a93a39afed390c8ff
DEXIZA <dexizaia@gmail.com>
2021-08-28 12:03:04 +0900

    エラー表示実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :create]
+  # ログイン・ログアウトで判定、除外ページ以外はトップへの処理
+
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
@@ -19,11 +22,4 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :home_num, :street_address, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
     end
-  # def purchase_record_params
-  #   params.permit(:price).merge(user_id: current_user.id)
-  # end
-
-  # def address_params
-  #   params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(donation_id: @donation.id)
-  # end
 end
